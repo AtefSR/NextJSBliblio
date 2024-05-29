714c813575dd8e820d1e8e9e3517c9e0baa53c87
AtefSR <sriouiatef@gmail.com>
2024-05-29 09:04:07 +0200

    V1

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import CarouselHome from "@/components/client/CarouselHome";
+import Footer from "@/components/client/Footer";
+import MainGridsHome from '@/components/client/mainGridsHome';
+
+export default function Home() {
+  return (
+    <>
+      <CarouselHome/>
+      <Footer/>
+      <MainGridsHome/>
+    </>
+  );
+}
